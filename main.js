6dfc172f487877712c72954b8a1c9861a8c75ca7
Manjunath.Hosamani <manjunathhosamani67@gmail.com>
2024-01-06 21:21:26 +0530

    Final commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const AdviseId = document.getElementById("adviseID");
+const advise = document.getElementById("advise");
+const btn = document.getElementById("btn-sub");
+const URL = "https://api.adviceslip.com/advice";
+
+
+function getAdvise(){
+    fetch(URL).then(response => response.json())
+    .then(data => displayData(data))
+    .catch(error => console.error("Error fetching data",error));
+    
+
+
+}
+
+function displayData(adiviseData){
+    AdviseId.textContent = adiviseData.slip.id;
+    console.log(adiviseData)
+    advise.textContent = adiviseData.slip.advice;
+}
+
+
+
+
+
+btn.addEventListener("click", getAdvise);
+
